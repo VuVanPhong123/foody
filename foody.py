3c62349a3550d7b8da4ad1de891e5662c0933dca
VuVanPhong123 <phongbeo124@gmail.com>
2025-02-21 09:16:09 +0700

    first commit

---
+++ foody.py
@@ -0,0 +1,19 @@
+import welcomeScreen
+from kivy.core.window import Window
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.config import Config
+# Set window size (width, height)
+Config.set('graphics', 'width', '350')
+Config.set('graphics', 'height', '600')
+
+# Disable resizing
+Config.set('graphics', 'resizable', False)
+from kivymd.uix.button import MDFlatButton, MDFloatingActionButton
+from kivymd.app import MDApp
+
+class foody(MDApp):
+    def build(self):
+        return welcomeScreen.WelcomeScreen()
